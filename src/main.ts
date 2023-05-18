449c774f925f160fc4b8eb02d35dd7f95c7051b2
adielsh <16068359+adielsh@users.noreply.github.com>
2023-05-18 22:48:45 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+});
+
+createApp(App).use(vuetify).mount('#app');
